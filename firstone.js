35eb5071657a080567f6f1232d7f452bf49c87ed
Ivan <i.loboda@dreamteam.gg>
2019-07-10 15:13:45 +0300

    first commit

---
+++ firstone.js
@@ -0,0 +1,62 @@
+'use strict';
+
+a = 5;
+clonsole.log(a);
+
+// var number = 5;
+// var string = "Hello!";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// console.log(4/0);
+// console.log('string'*9);
+ 
+// let something;
+// console.log(something);
+
+
+// let persone = {
+//     name: "Jonh",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[0])
+
+//  alert('Hello Dear User')
+
+// let answer = confirm('Are you ok??')
+
+// console.log(answer);
+
+// let answer = +prompt('Are you reach 18 y.o.?', 'Yes');
+
+// console.log(typeof(answer));
+
+// console.log('arr' + ' - object');
+// console.log(4 + +' - object');
+
+
+let incr = 10;
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+let isChecked = true,
+    isClose = false;
+
+    console.log(isChecked && isClose);
+
+console.log(isChecked || isClose);
+console.log(isChecked || !isClose);
